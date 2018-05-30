051cdbf060191e3cd093f4ca0692b24cd612e537
Eugene Sharygin <eush77@gmail.com>
2018-05-30 12:49:44 +0300

    [Mix] Refactor Mix class
    Add more state and split `buildSpecializer' into several functions.

--- lib/Transforms/Mix/Mix.cpp
+++ lib/Transforms/Mix/Mix.cpp
@@ -27,6 +27,7 @@
 #include "llvm/IR/GlobalValue.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/Intrinsics.h"
@@ -43,8 +44,10 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 
+#include <algorithm>
 #include <cassert>
 #include <iterator>
+#include <memory>
 #include <tuple>
 
 using namespace llvm;
@@ -66,21 +69,32 @@ public:
   bool runOnModule(Module &M) override;
 
 private:
-  // Resolve a metadata function id.
-  Function *resolveFunctionId(MetadataAsValue *FID) const {
-    return M->getFunction(cast<MDString>(FID->getMetadata())->getString());
-  }
-
   Function *createSpecializer(Function *F);
-  void buildSpecializer(Function *F, Function *SpecF) const;
-  void collectDynamicBlocks(BasicBlock *SBB,
-                            SetVector<BasicBlock *> &Blocks) const;
+  static FunctionType *getSpecializerType(FunctionType *FT);
+  void buildSpecializer();
+  void buildBasicBlock(BasicBlock *BB, BasicBlock *SBB);
+  void collectDynamicBlocks(BasicBlock *BB,
+                            SetVector<BasicBlock *> &DynBBs) const;
+  void buildInstruction(Instruction *I) const;
 
-  Module *M = nullptr;
-  const BindingTimeAnalysis *BTA = nullptr;
+  Module *M{};
+  Function *F{};
+  const BindingTimeAnalysis *BTA{};
+  Function *SF{};               // Specializer function.
+  std::unique_ptr<IRBuilder<>> B;
+  std::unique_ptr<mix::StagedIRBuilder<IRBuilder<>>> SB;
+
+  // A mapping from basic blocks of the original function to static basic
+  // blocks of the specializer.
+  MapVector<BasicBlock *, BasicBlock *> StaticBasicBlocks;
 };
 
 char Mix::ID;
+
+// Resolve a function by metadata id.
+Function *resolveFunctionId(const Module &M, MetadataAsValue *FID) {
+  return M.getFunction(cast<MDString>(FID->getMetadata())->getString());
+}
 
 } // namespace
 
@@ -105,8 +119,8 @@ bool Mix::runOnModule(Module &M) {
           continue;
         }
 
-        auto *Specializer = createSpecializer(
-            resolveFunctionId(cast<MetadataAsValue>(Intr->getArgOperand(1))));
+        auto *Specializer = createSpecializer(resolveFunctionId(
+            M, cast<MetadataAsValue>(Intr->getArgOperand(1))));
         auto *StagedContext = new BitCastInst(
             Intr->getArgOperand(0), mix::getContextPtrTy(M.getContext()),
             Intr->getArgOperand(0)->getName(), Intr);
@@ -130,9 +144,32 @@ bool Mix::runOnModule(Module &M) {
   return Changed;
 }
 
-namespace {
+Function *Mix::createSpecializer(Function *F) {
+  this->F = F;
+  BTA = &getAnalysis<BindingTimeAnalysis>(*F);
 
-FunctionType *getSpecializerType(FunctionType *FT) {
+  DEBUG(dbgs() << "Creating specializer for @" << F->getName() << '\n');
+
+  // Create specializer function.
+  SF = Function::Create(getSpecializerType(F->getFunctionType()),
+                        GlobalValue::PrivateLinkage,
+                        Twine(F->getName()) + ".mix");
+
+  // Name parameters of the specializer.
+  SF->arg_begin()->setName("context");
+  for (auto ArgIt = F->arg_begin(), SpecArgIt = SF->arg_begin() + 1;
+       ArgIt != F->arg_end(); ++ArgIt, ++SpecArgIt) {
+    SpecArgIt->setName(ArgIt->getName());
+  }
+
+  // Insert specializer function into the module.
+  M->getFunctionList().insertAfter(Module::iterator(F), SF);
+
+  buildSpecializer();
+  return SF;
+}
+
+FunctionType *Mix::getSpecializerType(FunctionType *FT) {
   SmallVector<Type *, 8> Params;
 
   Params.push_back(mix::getContextPtrTy(FT->getContext()));
@@ -142,95 +179,61 @@ FunctionType *getSpecializerType(FunctionType *FT) {
                            FT->isVarArg());
 }
 
-} // namespace
-
-Function *Mix::createSpecializer(Function *F) {
-  BTA = &getAnalysis<BindingTimeAnalysis>(*F);
-
-  DEBUG(dbgs() << "Creating specializer for @" << F->getName() << '\n');
-
-  // Create specializer function.
-  auto *SpecF = Function::Create(getSpecializerType(F->getFunctionType()),
-                                 GlobalValue::PrivateLinkage,
-                                 Twine(F->getName()) + ".mix");
-
-  // Name parameters of the specializer.
-  SpecF->arg_begin()->setName("context");
-  for (auto ArgIt = F->arg_begin(), SpecArgIt = SpecF->arg_begin() + 1;
-       ArgIt != F->arg_end(); ++ArgIt, ++SpecArgIt) {
-    SpecArgIt->setName(ArgIt->getName());
-  }
-
-  // Insert specializer function into the module.
-  M->getFunctionList().insertAfter(Module::iterator(F), SpecF);
-
-  buildSpecializer(F, SpecF);
-  return SpecF;
-}
-
-// Build code of the specializer function SpecC given the original function F.
-void Mix::buildSpecializer(Function *F, Function *SpecF) const {
-  IRBuilder<> Builder(BasicBlock::Create(SpecF->getContext(),
-                                         F->getEntryBlock().getName(), SpecF));
-  mix::StagedIRBuilder<decltype(Builder)> StagedBuilder(Builder,
-                                                        SpecF->arg_begin());
+// Build code of the specializer function.
+void Mix::buildSpecializer() {
+  B.reset(new IRBuilder<>(
+      BasicBlock::Create(SF->getContext(), F->getEntryBlock().getName(), SF)));
+  SB.reset(new mix::StagedIRBuilder<IRBuilder<>>(*B, SF->arg_begin()));
 
   // Build basic definitions for run-time code generator.
-  auto *StagedModule =
-      StagedBuilder.createModule(Twine(F->getName()) + ".module", "module");
-  auto *StagedFunction = StagedBuilder.createFunction(
+  auto *StagedModule = SB->createModule(Twine(F->getName()) + ".module", "module");
+  auto *StagedFunction = SB->createFunction(
       FunctionType::get(F->getReturnType(), false),
       GlobalValue::ExternalLinkage, F->getName(), StagedModule, "function");
-  StagedBuilder.createBuilder(StagedFunction, "builder");
+  SB->createBuilder(StagedFunction, "builder");
 
-  // A mapping from basic blocks of the original function to static basic
-  // blocks of the specializer.
-  MapVector<BasicBlock *, BasicBlock *> StaticBasicBlocks;
-  StaticBasicBlocks[&F->getEntryBlock()] = &SpecF->getEntryBlock();
+  StaticBasicBlocks.clear();
+  StaticBasicBlocks[&F->getEntryBlock()] = &SF->getEntryBlock();
 
   for (unsigned SBBNum = 0; SBBNum < StaticBasicBlocks.size(); ++SBBNum) {
+    BasicBlock *BB;
     BasicBlock *SBB;
-    BasicBlock *SpecSBB;
-    std::tie(SBB, SpecSBB) = StaticBasicBlocks.begin()[SBBNum];
+    std::tie(BB, SBB) = StaticBasicBlocks.begin()[SBBNum];
 
-    DEBUG(dbgs() << "  - Building static basic block %" << SBB->getName()
-          << '\n');
+    buildBasicBlock(BB, SBB);
+  }
 
-    Builder.SetInsertPoint(SpecSBB);
+  SB->disposeBuilder();
+  B->CreateRet(StagedModule);
+}
 
-    SetVector<BasicBlock *> BBs;
-    collectDynamicBlocks(SBB, BBs);
+void Mix::buildBasicBlock(BasicBlock *BB, BasicBlock *SBB) {
+  DEBUG(dbgs() << "  - Building static basic block %" << BB->getName() << '\n');
 
-    // Construct blocks in sequence.
-    for (auto *BB : BBs) {
-      StagedBuilder.positionBuilderAtEnd(StagedBuilder.stage(BB));
+  B->SetInsertPoint(SBB);
 
-      for (auto &I: *BB) {
-        switch (BTA->getBindingTime(&I)) {
-        case BindingTimeAnalysis::Static:
-          Builder.Insert(&I);
-          break;
+  SetVector<BasicBlock *> DynBBs;
+  collectDynamicBlocks(BB, DynBBs);
 
-        case BindingTimeAnalysis::Dynamic:
-          StagedBuilder.stage(&I);
-          break;
-        }
-      }
-    }
+  for (auto *DynBB : DynBBs) {
+    SB->positionBuilderAtEnd(SB->stage(DynBB));
 
-    if (BTA->getBindingTime(BBs.back()->getTerminator()) ==
-        BindingTimeAnalysis::Static) {
-      for (auto *SuccBB : successors(BBs.back())) {
-        if (!StaticBasicBlocks.count(SuccBB)) {
-          StaticBasicBlocks[SuccBB] =
-              BasicBlock::Create(SpecF->getContext(), SuccBB->getName(), SpecF);
-        }
+    std::for_each(DynBB->begin(), DynBB->end(),
+                  [this](Instruction &I) { buildInstruction(&I); });
+  }
+
+  BasicBlock *TermBB = DynBBs.back();
+
+  // Create new successor static blocks.
+  if (BTA->getBindingTime(TermBB->getTerminator()) ==
+      BindingTimeAnalysis::Static) {
+    for (auto *SuccBB : successors(TermBB)) {
+      if (!StaticBasicBlocks.count(SuccBB)) {
+        StaticBasicBlocks[SuccBB] =
+            BasicBlock::Create(SF->getContext(), SuccBB->getName(), SF);
       }
     }
   }
-
-  StagedBuilder.disposeBuilder();
-  Builder.CreateRet(StagedModule);
 }
 
 // Traverse dynamic blocks in the CFG starting from the given static block.
@@ -238,39 +241,52 @@ void Mix::buildSpecializer(Function *F, Function *SpecF) const {
 // The static block and all of dynamic blocks are added to the vector. The
 // last block added is the one ending with the static terminator for the
 // series.
-void Mix::collectDynamicBlocks(BasicBlock *SBB,
-                               SetVector<BasicBlock *> &Blocks) const {
-  assert(BTA->getBindingTime(SBB) == BindingTimeAnalysis::Static);
+void Mix::collectDynamicBlocks(BasicBlock *BB,
+                               SetVector<BasicBlock *> &DynBBs) const {
+  assert(BTA->getBindingTime(BB) == BindingTimeAnalysis::Static);
 
-  // If Blocks already contains SBB, move SBB to the end, so that we can start
-  // iteration at the last element assuming it is indeed SBB.
-  if (!Blocks.insert(SBB)) {
-    Blocks.remove(SBB);
-    Blocks.insert(SBB);
+  // If DynBBs already contains the starting block, move it to the end, so
+  // that we can start iteration at the of the collection.
+  if (!DynBBs.insert(BB)) {
+    DynBBs.remove(BB);
+    DynBBs.insert(BB);
   }
 
-  // Terminator block is set when discovered, and then pushed last.
-  BasicBlock *TerminatorBB = nullptr;
+  // The block containing the static terminator of the original basic block.
+  // This is added to DynBBs last.
+  BasicBlock *TermBB = nullptr;
 
-  for (unsigned BBNum = Blocks.size() - 1; BBNum < Blocks.size(); ++BBNum) {
-    BasicBlock *BB = Blocks[BBNum];
-    TerminatorInst *Term = BB->getTerminator();
+  for (unsigned BBNum = DynBBs.size() - 1; BBNum < DynBBs.size(); ++BBNum) {
+    BasicBlock *DynBB = DynBBs[BBNum];
+    TerminatorInst *Term = DynBB->getTerminator();
 
     // Check if static terminator is reached.
     if (BTA->getBindingTime(Term) == BindingTimeAnalysis::Static) {
-      assert(!TerminatorBB &&
+      assert(!TermBB &&
              "Multiple static terminators reachable from a static block");
-      TerminatorBB = BB;
+      TermBB = DynBB;
       continue;
     }
 
     for (BasicBlock *SuccBB : Term->successors()) {
-      Blocks.insert(SuccBB);
+      DynBBs.insert(SuccBB);
     }
   }
 
-  if (TerminatorBB) {
-    Blocks.insert(TerminatorBB);
+  if (TermBB) {
+    DynBBs.insert(TermBB);
+  }
+}
+
+void Mix::buildInstruction(Instruction *I) const {
+  switch (BTA->getBindingTime(I)) {
+  case BindingTimeAnalysis::Static:
+    B->Insert(I);
+    break;
+
+  case BindingTimeAnalysis::Dynamic:
+    SB->stage(I);
+    break;
   }
 }
 
@@ -278,9 +294,9 @@ INITIALIZE_PASS_BEGIN(Mix, "mix", "Multi-Stage Compilation", false, false)
 INITIALIZE_PASS_DEPENDENCY(BindingTimeAnalysis)
 INITIALIZE_PASS_END(Mix, "mix", "Multi-Stage Compilation", false, false)
 
-void llvm::addMixPass(PassManagerBuilder &Builder) {
+void llvm::addMixPass(PassManagerBuilder &PMB) {
   for (auto EP : {PassManagerBuilder::EP_EnabledOnOptLevel0,
                   PassManagerBuilder::EP_OptimizerLast}) {
-    Builder.addExtension(EP, [](const auto &, auto &PM) { PM.add(new Mix); });
+    PMB.addExtension(EP, [](const auto &, auto &PM) { PM.add(new Mix); });
   }
 }
